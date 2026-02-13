cd2989251c8727b8962908d54c624fa578ae171f
Jordan Mantel <jmantel@my.waketech.edu>
2026-02-13 13:27:30 -0500

    Initial commit

---
+++ js03_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Jordan Mantel
+     Date:   February 13th, 2026 (Fri)
+
+     Filename: js03.js
+ */
+
